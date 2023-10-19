6d7e9f73db9ccf1f52244e46f6763cab08cb479b
Atulkumar021 <91580986+Atulkumar021@users.noreply.github.com>
2023-10-19 18:11:59 +0530

    second

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,6 +2,6 @@ import { combineReducers, createStore } from "redux";
 import changetheNumber from '../Redux/reducers/countnum-reducer'
 import getbonusdata from '../Redux/reducers/bonusReducers'
 const rootReducer=combineReducers({changetheNumber,getbonusdata})
-const store=createStore(rootReducer)
+const store=createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
